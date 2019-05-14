e2d25fc8669f3a2f1d105156c5a20a2199fe065d
rgvh <rgvonhagel@gmail.com>
2019-05-14 15:12:47 -0700

    saving changes

--- code-challenges/301/for-each/challenges-01.test.js
+++ code-challenges/301/for-each/challenges-01.test.js
@@ -93,6 +93,10 @@ This function should use forEach again, but rather than taking in a callback as 
 This anonymous function should accept up to three arguments: the element, the index, and the array.
 ------------------------------------------------------------------------------------------------ */
 
+const myfunction = function() {
+  arr.forEach
+
+}
 const removeWithAnon = (arr) => {
   // let removeAnon = function (){
   //   arr.forEach(number) => {
